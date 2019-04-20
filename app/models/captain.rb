16b1df79560f9c4ff223f649fd81be2cc1e4edb5
Rafael Batista <rafaelb24@yahoo.com>
2019-04-20 02:20:20 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,17 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.catamaran_operators 
+    self.joins(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end 
+  
+  def self.sailors 
+    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end 
+  
+  def self.talented_seafarers 
+    binding.pry
+    # self.joins(boats: :classifications).where(classifications: [{name: "Sailboat" & "Motorboat"})
+    # self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).where(classifications: {name: "Motorboat"}).distinct
+  end
 end
