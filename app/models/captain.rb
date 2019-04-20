fa816a7838488b2cb2ca61d5e89e563d9406b188
Rafael Batista <rafaelb24@yahoo.com>
2019-04-20 02:25:12 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,7 +6,7 @@ class Captain < ActiveRecord::Base
   end 
   
   def self.sailors 
-    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    self.joins(boats: :classifications).where(classifications: []).distinct
   end 
   
   def self.talented_seafarers 
