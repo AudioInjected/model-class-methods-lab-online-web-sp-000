b20b11f0bd07cd39ae67fa2211dc55946f3067fb
Rafael Batista <rafaelb24@yahoo.com>
2019-04-20 02:26:36 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,12 +6,12 @@ class Captain < ActiveRecord::Base
   end 
   
   def self.sailors 
-    self.joins(boats: :classifications).where(classifications: [{name: "Sailboat"}]).distinct
+    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end 
   
   def self.talented_seafarers 
     binding.pry
-    self.joins(boats: :classifications).where(classifications: [{name: "Sailboat"}])
+    self.joins(boats: :classifications).where(classifications: [las])
     # self.joins(boats: :classifications).where(classifications: [{name: "Sailboat" & "Motorboat"})
     # self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).where(classifications: {name: "Motorboat"}).distinct
   end
