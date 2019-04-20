6e509c27cd8ed71faecad7dee382b9ba949f35ed
Rafael Batista <rafaelb24@yahoo.com>
2019-04-20 03:07:45 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,5 +2,7 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
   
-  \d
+  self.my_all 
+    self.all.distinct
+  end 
 end
